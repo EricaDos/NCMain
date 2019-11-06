cc26b35fe1bc95c0ac2f9b8f805253cc0a4c3165
Dos <ericadossantos2277@gmail.com>
2019-11-06 17:52:40 +0000

    Wifi Manager Currently working. Can't seem to view the RTC Time on the Serial Print, however, no errors.

--- NCMain.ino
+++ NCMain.ino
@@ -1,36 +1,137 @@
+
+//WiFi Manager Setup
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+//Initialising RTC Library
 #include <Wire.h>       //I2C library
 #include <RtcDS3231.h>  //RTC library
+
+
  
-//RtcDS3231 rtcObject;              //Uncomment for version 1.0.1 of the rtc library
-RtcDS3231<TwoWire> rtcObject(Wire); //Uncomment for version 2.0.0 of the rtc library
+//RtcDS3231 Rtc;              //Uncomment for version 1.0.1 of the rtc library
+RtcDS3231<TwoWire> Rtc(Wire); //Uncomment for version 2.0.0 of the rtc library
  
 void setup() {
  
   Serial.begin(115200);  //Starts serial connection
-  rtcObject.Begin();     //Starts I2C
+  WiFiManager wifiManager; //Starts wifimanager
+  wifiManager.autoConnect("AP-NAME", "AP-PASSWORD"); //Access Point and Password
+//  wifiManager.autoConnect(); for auto connect
+
+  Serial.print("RTC Compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+
+  
+  Rtc.Begin();     //Starts I2C
  
-  RtcDateTime currentTime = RtcDateTime(16, 05, 18, 21, 20, 0); //define date and time object
-  rtcObject.SetDateTime(currentTime); //configure the RTC with object
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
+
+ if (!Rtc.IsDateTimeValid()) 
+    {
+        if (Rtc.LastError() != 0)
+        {
+            // we have a communications error
+            // see https://www.arduino.cc/en/Reference/WireEndTransmission for 
+            // what the number means
+            Serial.print("RTC communications error = ");
+            Serial.println(Rtc.LastError());
+        }
+        else
+        {
+            // Common Causes:
+            //    1) first time you ran and the device wasn't running yet
+            //    2) the battery on the device is low or even missing
+
+            Serial.println("RTC lost confidence in the DateTime!");
+
+            // following line sets the RTC to the date & time this sketch was compiled
+            // it will also reset the valid flag internally unless the Rtc device is
+            // having an issue
+
+            Rtc.SetDateTime(compiled);
+        }
+    }
+
+    if (!Rtc.GetIsRunning())
+    {
+        Serial.println("RTC was not actively running, starting now");
+        Rtc.SetIsRunning(true);
+    }
+        RtcDateTime now = Rtc.GetDateTime();
+    if (now < compiled) 
+    {
+        Serial.println("RTC is older than compile time!  (Updating DateTime)");
+        Rtc.SetDateTime(compiled);
+    }
+    else if (now > compiled) 
+    {
+        Serial.println("RTC is newer than compile time. (this is expected)");
+    }
+    else if (now == compiled) 
+    {
+        Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+    }
+
+    // never assume the Rtc was last configured by you, so
+    // just clear them to your needed state
+    Rtc.Enable32kHzPin(false);
+    Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone); 
  
 }
  
 void loop() {
- 
-  RtcDateTime currentTime = rtcObject.GetDateTime();    //get the time from the RTC
- 
-  char str[20];   //declare a string as an array of chars
- 
-  sprintf(str, "%d/%d/%d %d:%d:%d",     //%d allows to print an integer to the string
-          currentTime.Year(),   //get year method
-          currentTime.Month(),  //get month method
-          currentTime.Day(),    //get day method
-          currentTime.Hour(),   //get hour method
-          currentTime.Minute(), //get minute method
-          currentTime.Second()  //get second method
-         );
- 
-  Serial.println(str); //print the string to the serial port
- 
-  delay(10000); //20 seconds delay
+ if (!Rtc.IsDateTimeValid()) 
+    {
+        if (Rtc.LastError() != 0)
+        {
+            // we have a communications error
+            // see https://www.arduino.cc/en/Reference/WireEndTransmission for 
+            // what the number means
+            Serial.print("RTC communications error = ");
+            Serial.println(Rtc.LastError());
+        }
+        else
+        {
+            // Common Causes:
+            //    1) the battery on the device is low or even missing and the power line was disconnected
+            Serial.println("RTC lost confidence in the DateTime!");
+        }
+    }
+
+    RtcDateTime now = Rtc.GetDateTime();
+    printDateTime(now);
+    Serial.println();
+
+ RtcTemperature temp = Rtc.GetTemperature();
+  temp.Print(Serial);
+  // you may also get the temperature as a float and print it
+    // Serial.print(temp.AsFloatDegC());
+    Serial.println("C");
+
+    delay(10000); // ten seconds
+}
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            dt.Month(),
+            dt.Day(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+    Serial.print(datestring);
  
 }
